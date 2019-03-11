ffe0295122b1cba553b8f685d7f98083b58a96c1
juanhernandez17 <juanhernandez17@my.unt.edu>
2019-03-11 00:11:13 -0500

    sec5

--- src/features/events/EventList/EventListItem.jsx
+++ src/features/events/EventList/EventListItem.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-import { Segment, Item, Icon, List, Button } from 'semantic-ui-react'
-import EventListAttendee from "./EventsListAttendee"
+import React, { Component } from "react";
+import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
+import EventListAttendee from "./EventsListAttendee";
 
 class EventListItem extends Component {
   render() {
-    const {event} = this.props;
+    const { event, onEventOpen, deleteEvent } = this.props;
     return (
-     <Segment.Group>
+      <Segment.Group>
         <Segment>
           <Item.Group>
             <Item>
@@ -28,17 +28,31 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-          {event.attendees && event.attendees.map((attendee) => (
-            <EventListAttendee key={attendee.id} attendee={attendee}/>
-          ))}
+            {event.attendees &&
+              event.attendees.map(attendee => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
           </List>
         </Segment>
         <Segment clearing>
-            <span>{event.description}</span>
-          <Button as="a" color="teal" floated="right" content="View" />
+          <span>{event.description}</span>
+          <Button
+            onClick={deleteEvent(event.id)}
+            as="a"
+            color="red"
+            floated="right"
+            content="Delete"
+          />
+          <Button
+            onClick={onEventOpen(event)}
+            as="a"
+            color="teal"
+            floated="right"
+            content="View"
+          />
         </Segment>
       </Segment.Group>
-    )
+    );
   }
 }
 
--- src/features/events/EventsDash/eventDash.jsx
+++ src/features/events/EventsDash/eventDash.jsx
@@ -1,100 +1,143 @@
-import React, { Component } from 'react'
-import { Grid, Button } from 'semantic-ui-react'
-import EventList from '../EventList/EventList'
-import EventForm from '../EventForm/EventForm'
-import cuid from 'cuid'
+import React, { Component } from "react";
+import { Grid, Button } from "semantic-ui-react";
+import EventList from "../EventList/EventList";
+import EventForm from "../EventForm/EventForm";
+import cuid from "cuid";
 const eventsD = [
   {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
-    category: 'culture',
+    id: "1",
+    title: "Trip to Tower of London",
+    date: "2018-03-27",
+    category: "culture",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+    city: "London, UK",
     venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    hostedBy: "Bob",
+    hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
     attendees: [
       {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        id: "a",
+        name: "Bob",
+        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
       },
       {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        id: "b",
+        name: "Tom",
+        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
       }
     ]
   },
   {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
-    category: 'drinks',
+    id: "2",
+    title: "Trip to Punch and Judy Pub",
+    date: "2018-03-28",
+    category: "drinks",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+    city: "London, UK",
+    venue: "Punch & Judy, Henrietta Street, London, UK",
+    hostedBy: "Tom",
+    hostPhotoURL: "https://randomuser.me/api/portraits/men/22.jpg",
     attendees: [
       {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        id: "b",
+        name: "Tom",
+        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
       },
       {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        id: "a",
+        name: "Bob",
+        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
       }
     ]
   }
-]
-
+];
 
 class EventDash extends Component {
   state = {
     events: eventsD,
     isOpen: false
-  }
+  };
 
   handleFormOpen = () => {
     this.setState({
-      isOpen: true
-    })
-  }
+      isOpen: true,
+      selectedEvent: null
+    });
+  };
 
   handleCancel = () => {
     this.setState({
       isOpen: false
-    })
-  }
+    });
+  };
 
-  handleCreateEvent = (newEvent) => {
+  handleUpdateEvent = updatedEvent => {
+    this.setState({
+      events: this.state.events.map(event => {
+        if (event.id === updatedEvent.id) {
+          return Object.assign({}, updatedEvent);
+        } else {
+          return event;
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null
+    });
+  };
+
+  handleOpenEvent = eventToOpen => () => {
+    this.setState({
+      selectedEvent: eventToOpen,
+      isOpen: true
+    });
+  };
+
+  handleCreateEvent = newEvent => {
     newEvent.id = cuid();
-    newEvent.hostPhotoURL = '/assets/user.png';
+    newEvent.hostPhotoURL = "/assets/user.png";
     const updatedEvents = [...this.state.events, newEvent];
     this.setState({
       events: updatedEvents,
       isOpen: false
-    })
-  }
+    });
+  };
+
+  handleDeleteEvent = eventId => () => {
+    const updatedEvents = this.state.events.filter(e => e.id !== eventId);
+    this.setState({
+      events: updatedEvents
+    });
+  };
 
   render() {
+    const { selectedEvent } = this.state;
     return (
-        <Grid>
-            <Grid.Column width={10}>
-                <EventList events={this.state.events} />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <Button onClick={this.handleFormOpen} positive content='Create Event'/>
-                {this.state.isOpen && 
-                <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel}/>}
-            </Grid.Column>
-        </Grid>
+      <Grid>
+        <Grid.Column width={10}>
+          <EventList
+            deleteEvent={this.handleDeleteEvent}
+            onEventOpen={this.handleOpenEvent}
+            events={this.state.events}
+          />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <Button
+            onClick={this.handleFormOpen}
+            positive
+            content="Create Event"
+          />
+          {this.state.isOpen && (
+            <EventForm
+              updateEvent={this.handleUpdateEvent}
+              selectedEvent={selectedEvent}
+              createEvent={this.handleCreateEvent}
+              handleCancel={this.handleCancel}
+            />
+          )}
+        </Grid.Column>
+      </Grid>
     );
   }
 }
